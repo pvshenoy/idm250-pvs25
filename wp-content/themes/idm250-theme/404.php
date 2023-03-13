20024f0ae73421dc4218edb793e63537ae947f64
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2023-03-13 08:03:46 -0400

    404 page

--- wp-content/themes/idm250-theme/404.php
+++ wp-content/themes/idm250-theme/404.php
@@ -1,6 +1,7 @@
 <?php get_header('404'); ?>
 <div class="error-img">
-  <img src="dist/images/404.png" alt="" class="error">
+  <img src="<?php echo get_template_directory_uri(); ?>dist/images/404.png" alt="">
+  <!-- <img src="dist/images/404.png" alt="" class="error"> -->
 </div>
 <h2 class="error-text">Oops! This page couldn't be found.</h2>
 <?php get_footer('simple'); ?>
