0e81e0bb082faeccf56edd81c19cc49cf9d52eed
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2023-03-24 03:32:37 -0400

    so much going on

--- wp-content/themes/idm250-theme/components/featured-block.php
+++ wp-content/themes/idm250-theme/components/featured-block.php
@@ -1,36 +1,11 @@
 <!-- Start ./components/featured-block -->
-<section aria-labelledby="social-impact-heading" class="mx-auto max-w-7xl px-4 pt-24 sm:px-6 sm:pt-32 lg:px-8">
-  <div class="relative overflow-hidden rounded-lg">
-    <div class="absolute inset-0">
-      <?php
-      $image = get_field('cta_background_image');
-      if(!empty($image)): ?>
-      <img src="<?php echo esc_url($image['url']); ?>" class=" h-full w-full object-cover object-center"
-        alt="<?php echo esc_attr($image['alt']); ?>" />
-      <?php endif; ?>
-    </div>
-    <div class="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
-      <div class="relative mx-auto flex max-w-3xl flex-col items-center text-center">
-        <h2 id="social-impact-heading" class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-          <?php echo get_field('cta_heading'); ?>
-        </h2>
-        <p class="mt-3 text-xl text-white">
-          <?php echo get_field('cta_description'); ?>
-        </p>
-        <?php
-        $link = get_field('cta_link');
-      if($link):
-          $link_url = $link['url'];
-          $link_title = $link['title'];
-          $link_target = $link['target'] ? $link['target'] : '_self';
-          ?>
-        <a class="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-          href="<?php echo esc_url($link_url); ?>"
-          target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-        <?php endif; ?>
-
-      </div>
-    </div>
+<!-- <section class="biography">
+  <img src="http://localhost:8888/wp-content/uploads/2023/03/old-gelato.png" alt="" class="biography-img">
+  <div class="bio-text">
+    <h2 class="bio-heading">From Ancient Rome to Your Taste Buds</h2>
+    <p class="bio-description">
+      Gelato has a long and rich history dating back to ancient Rome, where emperors would send runners to the mountains to collect snow and ice to mix with fruit juices and honey. The modern version of gelato, as we know it today, originated in Italy during the Renaissance period, when the Medici family held a contest to find the best frozen dessert. In the 17th century, gelato became more widespread in Italy and was served in cafes and gelaterias across the country. Over the years, gelato has evolved and grown in popularity, with new flavors and techniques constantly being developed. 
+      Today, it is enjoyed all over the world and is considered a symbol of Italian culinary excellence.</p>
   </div>
-</section>
+</section> -->
 <!-- End ./components/featured-block -->
--- wp-content/themes/idm250-theme/front-page.php
+++ wp-content/themes/idm250-theme/front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <?php 
 get_template_part('components/heros/hero-home');
+get_template_part('components/featured-block');
 get_template_part('components/content'); ?>
 <?php get_footer(); ?>
