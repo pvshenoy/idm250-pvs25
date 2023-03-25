46e14cf692f8b00b64819b321bbc782d5beada37
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2023-03-25 03:32:53 -0400

    footer fix

--- wp-content/themes/idm250-theme/components/footer.php
+++ wp-content/themes/idm250-theme/components/footer.php
@@ -1,5 +1,7 @@
 <footer>
+  <div class="footer">
   <?php wp_nav_menu(['theme_location' => 'footer-menu']);?>
+  </div>
   <p class=footer-content>&copy; <?php echo date('Y'); ?>
     <?php bloginfo('name'); ?>
   </p>
