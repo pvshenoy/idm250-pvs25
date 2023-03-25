d95a91f2f9c0cd57a31bd48a81453b5e6c58f258
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2023-03-24 22:02:57 -0400

    css test

--- wp-content/themes/idm250-theme/components/header.php
+++ wp-content/themes/idm250-theme/components/header.php
@@ -1,11 +1,11 @@
 <header> 
 <div class="header-desktop">
     <a href="<?php echo '$home'?>"><h3 class="site-title">gelato</h3></a>
-    <ul class="desktop-nav">
+    <nav class="desktop-nav">
         <?php
         wp_nav_menu (['theme_location' => 'primary-menu']);
         ?>
-    </ul>
+    </nav>
 </div>
 <hr class="nav-linebreak">
 
